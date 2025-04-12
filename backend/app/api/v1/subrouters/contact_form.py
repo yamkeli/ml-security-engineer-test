683d5d8faeeb101f51780471701712fc8c1f5466
yamkeli <yamkeli.work@gmail.com>
2025-04-13 06:55:42 +0800

    Added loading form feature
    Included decrypting from DB

--- backend/app/api/v1/subrouters/contact_form.py
+++ backend/app/api/v1/subrouters/contact_form.py
@@ -14,6 +14,7 @@ from app.core.auth.jwt import (
 from app.core.crypto import encrypt, decrypt
 
 from app.core.contact import form_handling
+from app.core.contact.form_handling import UniqueViolation
 
 from app.schema.form import UserContactForm
 from app.schema.jwt import JWTAuthResult
@@ -46,9 +47,37 @@ def submit_form(
 
     # encrypt ssn, get encrypted ssn and encrypted dek
     ssn_enc, dek_enc = encrypt.encryption(ssn)
-
-    form_handling.store_contact_info(
-        name, dob, email, ssn_enc, dek_enc, user_id, database_manager
-    )
+    try:
+        form_handling.store_contact_info(
+            name, dob, email, ssn_enc, dek_enc, user_id, database_manager
+        )
+    except UniqueViolation as e:
+        raise HTTPException(409, {"status": "Contact info already uploaded."})
 
     return {"status": "Contact Info Uploaded"}
+
+
+@contact_router.get("/load", status_code=200)
+def load_contact_info(
+    database_manager: DatabaseManager = Depends(get_db_manager),
+    jwt_auth: JWTAuthResult = Depends(jwt.wrapped_verify_jwt),
+):
+    # jwt state check
+    if not all(jwt_auth.payload.model_dump().values()) or jwt_auth.error != None:
+        if isinstance(jwt_auth.error, ExpiredSignatureError):
+            raise HTTPException(401, {"status": "Token is expired."})
+        if isinstance(jwt_auth.error, DecodeError):
+            raise HTTPException(400, {"status": "Error in decoding token."})
+        if isinstance(jwt_auth.error, (InvalidAlgorithmError, ExpiredSignatureError)):
+            raise HTTPException(401, {"status": "Token is invalid."})
+    # from verified jwt
+    user_id = jwt_auth.payload.sub
+
+    # retrieve info
+    name, email, dob, ssn_enc, dek_enc, user_id = form_handling.load_contact_info(
+        user_id, database_manager
+    )
+
+    ssn = decrypt.decryption(bytes(ssn_enc), bytes(dek_enc))
+
+    return {"name": name, "email": email, "dob": dob, "ssn": ssn}
